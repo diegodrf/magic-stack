1b63cf46384137b8d4fbec8a73eeb618e3b4dcf8
Diego Faria <diego.rdfaria@gmail.com>
2018-11-21 22:49:06 -0200

    Corrigido o problema de criação do dicionário. Agora está sendo criado da forma correta. Removido o import json, agora desnecessário.

--- magic.py
+++ magic.py
@@ -1,21 +1,12 @@
 import requests
 from sys import argv
-import json
 
 # script de comunicação universal para o magic_stack.
 
 # Recebe os argumentos independente do tamanho e monta um dicionário chave:valor.
-j = '{'
+j = {}
 for i in range(1, len(argv)):
-    if i < len(argv) - 1:
-        # Se argumento não for o último, adiciona vírgula no final.
-        j += '"{}":"{}",'.format(i, argv[i])
-    else:
-        # Se for o último argumento, não adiciona a vírgula.
-        j += '"{}":"{}"'.format(i, argv[i])
-j += '}'
-
-j = json.loads(j)
+    j[str(i)] = argv[i]
 
 # Envia um post para a URL informada no primero campo (Este item é obrigatório) e adiciona o json no corpo.
 res = requests.post(j['1'], data=j)
